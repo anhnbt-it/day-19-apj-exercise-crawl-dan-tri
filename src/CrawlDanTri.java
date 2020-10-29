0ea014c20b8a036e8d24be77713631caa8b46eff
anhnbt-it <anhnbt.it@gmail.com>
2020-10-29 15:07:32 +0700

    first commit

---
+++ src/CrawlDanTri.java
@@ -0,0 +1,33 @@
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: AnhNBT (anhnbt.it@gmail.com)
+ * Date: 10/29/2020
+ * Time: 2:40 PM
+ */
+public class CrawlDanTri {
+    public static void main(String[] args) {
+        try {
+            URL url = new URL("https://dantri.com.vn/the-gioi.htm");
+            Scanner scanner = new Scanner(new InputStreamReader(url.openStream()));
+            scanner.useDelimiter("\\Z");
+            String content = scanner.next();
+//            content = content.replaceAll("\\n+", "");
+            Pattern pattern = Pattern.compile("<a\\sdata-utm=\"Cate_TheGioi\\|MainList\\|1.*\"\\stitle=\"(.*?)\"");
+            Matcher matcher = pattern.matcher(content);
+            while (matcher.find()) {
+                System.out.println(matcher.group(1));
+            }
+            scanner.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
